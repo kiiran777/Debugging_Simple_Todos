27df292ca9a97522556ab74dc76443f0c2a633cf
kiran dwivedi <kirandwivedi530gmail.com>
2023-03-24 02:42:49 +0000

    Debugging Simple

---
+++ src/components/TodoItem/index.js
@@ -0,0 +1,22 @@
+import './index.css'
+import React from 'react'
+
+const TodoItem = props => {
+  const {todoDetails, deleteTodo} = props
+  const {id, title} = todoDetails
+
+  const onDeleteTodo = () => {
+    deleteTodo(id)
+  }
+
+  return (
+    <li>
+      <p className="title">{title}</p>
+      <button type="button" className="delete-btn" onClick={onDeleteTodo}>
+        Delete
+      </button>
+    </li>
+  )
+}
+
+export default TodoItem
